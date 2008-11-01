8669e67fe8a67e3ae94f924945490f4b57197413
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2008-11-01 13:28:08 +0000

    added action for save
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@18412 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ src/com/versionone/integration/idea/actions/SaveData.java
@@ -0,0 +1,14 @@
+package com.versionone.integration.idea.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ *
+ */
+public class SaveData extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("Save.actionPerformed()");
+        // TODO: insert action logic here
+    }
+}
