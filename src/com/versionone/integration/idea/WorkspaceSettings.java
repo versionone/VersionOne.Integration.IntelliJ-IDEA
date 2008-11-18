901225639f05bbbc082e3c2f53f30631b9c8f5e2
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2008-11-18 11:27:06 +0000

    fitted project for working under the mac os
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@18852 542498e7-56f8-f544-a0f2-a85913ac812a

--- src/com/versionone/integration/idea/WorkspaceSettings.java
+++ src/com/versionone/integration/idea/WorkspaceSettings.java
@@ -1,12 +1,14 @@
 package com.versionone.integration.idea;
 
+import com.intellij.openapi.components.PersistentStateComponent;
+
 /*
 @State(name = "V1PluginSettings", storages = {
         @Storage(id = "other",
                 file = "$WORKSPACE_FILE$"
         )})
 */
-public class WorkspaceSettings {//implements PersistentStateComponent<WorkspaceSettings> {
+public class WorkspaceSettings implements PersistentStateComponent<WorkspaceSettings> {
     public String v1Path = "http://jsdksrv01/VersionOne/";
     public String user = "admin";
     public String passwd = "admin";
