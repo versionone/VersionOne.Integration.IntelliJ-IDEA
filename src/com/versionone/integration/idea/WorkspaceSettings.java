ac27dacfab9f2cba5cf863d1a209c04308e082c1
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2008-11-18 15:04:26 +0000

    made small update setting system
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@18868 542498e7-56f8-f544-a0f2-a85913ac812a

--- src/com/versionone/integration/idea/WorkspaceSettings.java
+++ src/com/versionone/integration/idea/WorkspaceSettings.java
@@ -3,6 +3,7 @@ package com.versionone.integration.idea;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 
 
 @State(name = "V1PluginSettings", storages = {
@@ -18,9 +19,9 @@ public class WorkspaceSettings implements PersistentStateComponent<WorkspaceSett
 
     private static WorkspaceSettings setting = new WorkspaceSettings();
 
-    // singelton
-    private WorkspaceSettings() {
-    }
+//    // singelton
+//    private WorkspaceSettings() {
+//    }
 
     public WorkspaceSettings getState() {
         return this;
@@ -32,6 +33,8 @@ public class WorkspaceSettings implements PersistentStateComponent<WorkspaceSett
         v1Path = state.v1Path;
         projectName = state.projectName;
         isShowAllTask = state.isShowAllTask;
+        //XmlSerializerUtil.copyBean(state, this);
+
     }
 
     public static WorkspaceSettings getInstance() {
