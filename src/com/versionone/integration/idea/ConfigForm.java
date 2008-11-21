9f3fdce59bbb6ef6b05a2fa0f947088754fb8294
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2008-11-21 19:37:42 +0000

    Config form stub
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@19011 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ src/com/versionone/integration/idea/ConfigForm.java
@@ -0,0 +1,10 @@
+package com.versionone.integration.idea;
+
+import javax.swing.*;/*(c) Copyright 2008, VersionOne, Inc. All rights reserved. (c)*/
+
+public class ConfigForm {
+    private JTextField serverUrl;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton validateConnectionButton;
+}
