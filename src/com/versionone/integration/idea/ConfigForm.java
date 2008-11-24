1bf060f5b188a0d5915b85b08bea17b5747eca71
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2008-11-24 10:26:22 +0000

    added template for settings
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@19052 542498e7-56f8-f544-a0f2-a85913ac812a

--- src/com/versionone/integration/idea/ConfigForm.java
+++ src/com/versionone/integration/idea/ConfigForm.java
@@ -1,10 +1,117 @@
+/*(c) Copyright 2008, VersionOne, Inc. All rights reserved. (c)*/
 package com.versionone.integration.idea;
 
-import javax.swing.*;/*(c) Copyright 2008, VersionOne, Inc. All rights reserved. (c)*/
+import com.intellij.openapi.options.UnnamedConfigurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.ui.Messages;
 
-public class ConfigForm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ConfigForm implements UnnamedConfigurable {
     private JTextField serverUrl;
     private JTextField textField1;
     private JTextField textField2;
     private JButton validateConnectionButton;
+    private JPanel generalPanel;
+    private WorkspaceSettings settings;
+
+    public ConfigForm(WorkspaceSettings settings) {
+        validateConnectionButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Messages.showWarningDialog("Button pressed", e.paramString());
+            }
+        });
+        validateConnectionButton.setEnabled(false);
+
+        this.settings = settings;
+
+        reset();
+    }
+
+    public Component getContentPanel() {
+        return generalPanel;
+    }
+
+    public JComponent createComponent() {
+        //N/A
+        return null;
+    }
+
+    public boolean isModified() {
+        boolean result;
+
+        result = !serverUrl.getText().equals(settings.v1Path);
+        result = result || !textField1.getText().equals(settings.user);
+        result = result || !textField2.getText().equals(settings.passwd);
+
+        validateConnectionButton.setEnabled(result);
+
+
+        return result;
+    }
+
+//    public boolean isProjectViewStyleChanged() {
+////        final RApplicationSettings settings = RApplicationSettings.getInstance();
+////        return settings.useRubySpecificProjectView != useRubyProjectViewBox.isSelected();
+//        return true;
+//    }
+
+    public void apply() throws ConfigurationException {
+
+        settings.v1Path = serverUrl.getText();
+        settings.user = textField1.getText();
+        settings.passwd = textField2.getText();
+
+//        final RApplicationSettings settings = RApplicationSettings.getInstance();
+//        settings.useConsoleOutputOtherFilters = otherFiltersCheckBox.isSelected();
+//        settings.useConsoleOutputRubyStacktraceFilter = rubyStacktraceFilterCheckBox.isSelected();
+//        settings.useConsoleColorMode = colorModeCheckBox.isSelected();
+//        settings.additionalEnvPATH = myTFAdditioanlPath.getText().trim();
+//
+//        final boolean isProjectViewStyleChanged = isProjectViewStyleChanged();
+//        settings.useRubySpecificProjectView = useRubyProjectViewBox.isSelected();
+//        if (isProjectViewStyleChanged){
+//            final int result = Messages.showYesNoDialog(myContentPane.getParent(),
+//                    RBundle.message("settings.plugin.general.tab.use.ruby.project.view.changed.message"),
+//                    RBundle.message("settings.plugin.general.tab.use.ruby.project.view.changed.title"),
+//                    Messages.getQuestionIcon());
+//            if (result == DialogWrapper.OK_EXIT_CODE){
+//                ApplicationManager.getApplication().invokeLater(new Runnable() {
+//                    public void run() {
+//                        // reload all projects
+//                        for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+//                            ProjectManager.getInstance().reloadProject(project);
+//                        }
+//                    }
+//                }, ModalityState.NON_MODAL);
+//            }
+//        }
+    }
+
+    public void reset() {
+
+        serverUrl.setText(settings.v1Path);
+        textField1.setText(settings.user);
+        textField2.setText(settings.passwd);
+
+//        final RApplicationSettings settings = RApplicationSettings.getInstance();
+//        rubyStacktraceFilterCheckBox.setSelected(settings.useConsoleOutputRubyStacktraceFilter);
+//        otherFiltersCheckBox.setSelected(settings.useConsoleOutputOtherFilters);
+//        colorModeCheckBox.setSelected(settings.useConsoleColorMode);
+//        useRubyProjectViewBox.setSelected(settings.useRubySpecificProjectView);
+//        myTFAdditioanlPath.setText(settings.additionalEnvPATH);
+//
+//        myTPIdeaPath.setText(System.getenv(OSUtil.getPATHenvVariableName()));
+    }
+
+    public void disposeUIResources() {
+        //Do nothing
+    }
+
+    public JPanel getPanel() {
+        return generalPanel;
+    }
 }
