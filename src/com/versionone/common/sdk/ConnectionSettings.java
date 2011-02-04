ad557d3802300609f3b8bb39fe9451042a2b50b9
Mikhail <Mikhail@542498e7-56f8-f544-a0f2-a85913ac812a>
2011-02-04 15:02:22 +0000

    added missed file
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/IDEA@40508 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ src/com/versionone/common/sdk/ConnectionSettings.java
@@ -0,0 +1,15 @@
+package com.versionone.common.sdk;
+
+/**
+ * Setting for connection to VersionOne
+ */
+public class ConnectionSettings {
+    public String v1Path;
+    public String v1Username;
+    public String v1Password;
+    public boolean isWindowsIntegratedAuthentication;
+    public boolean isProxyEnabled;
+    public String proxyUsername;
+    public String proxyPassword;
+    public String proxyUri;
+}
